4e76d2e6991db466082d70e7c5debbdba9e38bda
nicolezhou05 <nzhou30@stuy.edu>
2021-10-03 21:52:32 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,5 +1,20 @@
+/*Nicole Zhou
+APCS
+HW08 Refactor Freshie Zero
+2021-09-30
+
+Discoveries:
+Unresolved Questions:
+*/
+
 public class Greet {
 	public static void main(String[] args){
-		System.out.println("No hablo queso");
+		greet("Foo");
+		greet("Moo");
+		greet("Boo");
+	}
+
+	public static void greet(String name){
+		System.out.println("Why, hello there, " + name + ". How do you do?");
 	}
 }
