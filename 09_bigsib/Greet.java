a40d7bc9421eedd3ea332422aae85d4d2ce5b516
nicolezhou05 <nzhou30@stuy.edu>
2021-10-03 22:16:44 -0400

    Update 09_bigsib

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,17 @@
+// Nicole Zhou
+// APCS
+// HW09 What are BigSibs Good For?
+// 2021-10-03
+
+/*
+Dicoveries:
+Unresolved Questions:
+*/
+
+public class Greet {
+	public static void main(String[] args) {
+		BigSib.greet("Film");
+		BigSib.greet("Flam");
+		BigSib.greet("Kazaam");
+	}
+}
