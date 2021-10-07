f35f02f8bb2413745b26c49d59be4594b43219fc
nicolezhou05 <nzhou30@stuy.edu>
2021-10-07 04:29:11 -0400

    Upload 13_hatch

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,12 @@
+public class BigSib {
+	private String helloMsg;
+	public BigSib() {
+		helloMsg = "Word up";
+}
+	private void setHelloMsg(String msg) {
+		helloMsg = msg;
+	}
+	public String greet(String name) {
+		return helloMsg + " " + name;
+	}
+}
---
+++ 13_hatch/Greet.java
@@ -0,0 +1,20 @@
+// Team Name: Noble Narwhals (Nicole Zhou, Duck, Nakib Abedin, Joker)
+// APCS Where do BigSibs Come From?
+// HW13
+// 2021-10-6
+
+/*
+DISCO:
+QCC:
+*/
+
+public class Greet {
+	public static void main(String[] args) {
+		String greeting;
+
+		BigSib richard = new BigSib();
+
+		greeting = richard.greet("freshman");
+		System.out.println(greeting);
+	}
+}
