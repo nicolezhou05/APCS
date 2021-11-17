cff809c8ed5cb8501d1213f051899acbbef7452f
nicolezhou05 <nzhou30@stuy.edu>
2021-11-17 11:36:30 -0500

    testing

---
+++ testing/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+  public static void main(String[] args){
+    Integer x = new Integer(3);
+    Integer y = 3;
+    System.out.println(x == y);
+    System.out.println(x.equals(y));
+  }
+}
