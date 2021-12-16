8284eeeec131db97eafe80d0337fd9fd7bc0449b
Nicole Z <78169696+nicolezhou05@users.noreply.github.com>
2021-12-16 06:02:26 -0500

    Update BinSearch.java

--- 48_search/BinSearch.java
+++ 48_search/BinSearch.java
@@ -1,72 +1,80 @@
-/**
-   class BinSearch
-   Binary search on array of Comparables
-**/
+// Team: CNN (Nicole Zhou, Duck, Nada Hameed, Ray, Corina Chen, Binktop)
+// APCS
+// HW48 (?)
+// 2021-12-15
+// time spent: 0.9 hr
 
-public class BinSearch
-{
+/*
+DICSO:
+1) When running a.compareTo(b), if a>b, it returns a positive num. If a<b, it returns a negative num. When a=b, it returns 0.
+2) a and b cannot be ints, but can be Strings and Integers.
+QCC:
+1) What causes bad operand types?
+2) When running this code, terminal tells me I have unchecked or unsafe operations. How can I fix that?
+*/
 
+public class BinSearch{
   /**
-     int binSearch(Comparable[],Comparable) -- searches an array of 
+     int binSearch(Comparable[],Comparable) -- searches an array of
      Comparables for target Comparable
      pre:  input array is sorted in ascending order
      post: returns index of target, or returns -1 if target not found
   **/
-  public static int binSearch ( Comparable[] a, Comparable target )
-  {
+  public static int binSearch ( Comparable[] a, Comparable target ){
     //uncomment exactly 1 of the 2 stmts below:
-
     return binSearchIter( a, target, 0, a.length-1 );
     //return binSearchRec( a, target, 0, a.length-1 );
   }
 
-
-  public static int binSearchRec( Comparable[] a,
-                                  Comparable target,
-                                  int lo, int hi )
-  {
-
+  public static int binSearchRec( Comparable[] a, Comparable target, int lo, int hi ){
     int tPos = -1; //init return var to flag value -1
-
     int m = (lo + hi) / 2; //init mid pos var
 
-
+    if (a[m].compareTo(target) < 0){
+      hi = m - 1;
+      binSearchRec(a, target, lo, hi);
+    }
+    else if (a[m].compareTo(target) > 0){
+      lo = m + 1;
+      binSearchRec(a, target, lo, hi);
+    }
+    else if (a[m].compareTo(target) == 0){
+      return m;
+    }
 
     return tPos;
   }//end binSearchRec
 
-
-  public static int binSearchIter( Comparable[] a,
-                                   Comparable target,
-                                   int lo, int hi )
-  {
-
+  public static int binSearchIter( Comparable[] a, Comparable target, int lo, int hi ){
     int tPos = -1; //init return var to flag value -1
     int m = (lo + hi) / 2; //init mid pos var
 
-    while( /* ? */ ) { // run until lo & hi cross
-
+    while(lo <= hi) { // run until lo & hi cross
       //update mid pos var
-
+      m = (lo + hi) / 2;
       // target found
-
+      if (a[m].compareTo(target) == 0){
+        return m;
+      }
       // value at mid index higher than target
-
+      else if (a[m].compareTo(target) > 0){
+        hi = m - 1;
+      }
       // value at mid index lower than target
-
+      else if (a[m].compareTo(target) < 0){
+        lo = m + 1;
+      }
     }
+
     return tPos;
   }//end binSearchIter
 
-
-
   //tell whether an array is sorted in ascending order
-  private static boolean isSorted( Comparable[] arr )
-  {
-
+  private static boolean isSorted( Comparable[] arr ){
     boolean retBoo = true; //init to true, assume array is sorted
 
     //Q: Why would a FOREACH loop not suffice here?
+    //A: You need to know the index.
     for( int i=0; i < arr.length-1; i++ ) {
       if ( ( arr[i].compareTo(arr[i+1]) > 0 ) ) {
         return false;
@@ -75,28 +83,18 @@ public class BinSearch
     return retBoo; //if entire array was traversed, it must be sorted
   }
 
-
   // utility/helper fxn to display contents of an array of Objects
-  private static void printArray( Comparable[] arr ) {
-    String output = "[ "; 
-
+  private static void printArray( Comparable[] arr ){
+    String output = "[ ";
     for( Comparable c : arr )
 	    output += c + ", ";
-
     output = output.substring( 0, output.length()-2 ) + " ]";
-
     System.out.println( output );
   }
 
-
-
   //main method for testing
   //minimal -- augment as necessary
-  public static void main ( String[] args )
-  {
-
-    /*----------------------------------------------------
-
+  public static void main ( String[] args ){
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     System.out.println("\nNow testing binSearch on Comparable array...");
 
@@ -115,9 +113,9 @@ public class BinSearch
     }
 
     printArray( iArr3 );
-    System.out.println( "iArr3 sorted? -- " + isSorted(iArr2) );
+    System.out.println( "iArr3 sorted? -- " + isSorted(iArr3) );
 
-    //search for 6 in array 
+    //search for 6 in array
     System.out.println( binSearch(iArr2,2) );
     System.out.println( binSearch(iArr2,4) );
     System.out.println( binSearch(iArr2,6) );
@@ -125,7 +123,7 @@ public class BinSearch
     System.out.println( binSearch(iArr2,13) );
     System.out.println( binSearch(iArr2,42) );
 
-    //search for 43 in array 
+    //search for 43 in array
     System.out.println( binSearch(iArr2,43) );
 
     System.out.println( "now testing binSearch on iArr3..." );
@@ -133,13 +131,11 @@ public class BinSearch
     System.out.println( binSearch(iArr3,8) );
     System.out.println( binSearch(iArr3,5) );
 
-    //search for 43 in array 
+    //search for 43 in array
     System.out.println( binSearch(iArr3,43) );
+    /*----------------------------------------------------
     ====================================================*/
 
-
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
   }//end main()
-
 }//end class BinSearch2
