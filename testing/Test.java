e49794dc037962fb9773ad93a9c500c0bc52258f
nicolezhou05 <nzhou30@stuy.edu>
2021-12-16 06:05:31 -0500

    testing...

--- testing/Test.java
+++ testing/Test.java
@@ -1,13 +1,3 @@
 public class Test {
-  int a = 251;
-  public static void main(String[] args) {
-    Main demo = new Main();
-    System.out.println("Value of a before: " + demo.a);
-    demo.change(demo.a);
-    System.out.println("Value of a after: " + demo.a);
-  }
 
-  public void change(int target) {
-    target += 100;
-  }
 }
