ab273b74d414e4f556b14f9a71b103ca691e1626
nicolezhou05 <nzhou30@stuy.edu>
2022-01-12 23:42:41 -0500

    fix indentation

--- cobo/magpie/activity2/Magpie2.java
+++ cobo/magpie/activity2/Magpie2.java
@@ -18,40 +18,40 @@
 
 public class Magpie2
 {
-	/**
-	 * Get a default greeting
-	 * @return a greeting
-	 */
-	public String getGreeting()
-	{
-		return "Hello, let's talk.";
-	}
+  /**
+   * Get a default greeting
+   * @return a greeting
+   */
+  public String getGreeting()
+  {
+    return "Hello, let's talk.";
+  }
 
-	/**
-	 * Gives a response to a user statement
-	 *
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
-	public String getResponse(String statement)
-	{
-		String response = "";
+  /**
+   * Gives a response to a user statement
+   *
+   * @param statement
+   *            the user statement
+   * @return a response based on the rules given
+   */
+  public String getResponse(String statement)
+  {
+    String response = "";
     if (statement.length() == 0)
     {
       response = "Say something, please.";
     }
-		else if (statement.indexOf("no") >= 0)
-		{
-			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
-		{
-			response = "Tell me more about your family.";
-		}
+    else if (statement.indexOf("no") >= 0)
+    {
+      response = "Why so negative?";
+    }
+    else if (statement.indexOf("mother") >= 0
+	|| statement.indexOf("father") >= 0
+	|| statement.indexOf("sister") >= 0
+	|| statement.indexOf("brother") >= 0)
+    {
+      response = "Tell me more about your family.";
+    }
     else if (statement.indexOf("dog") >= 0
         || statement.indexOf("cat") >= 0)
     {
@@ -73,40 +73,40 @@ public class Magpie2
     {
       response = "What do you mean why?";
     }
-		else
-		{
-			response = getRandomResponse();
-		}
-		return response;
-	}
+    else
+    {
+      response = getRandomResponse();
+    }
+    return response;
+  }
 
-	/**
-	 * Pick a default response to use if nothing else fits.
-	 * @return a non-committal string
-	 */
-	private String getRandomResponse()
-	{
-		final int NUMBER_OF_RESPONSES = 6;
-		double r = Math.random();
-		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-		String response = "";
+  /**
+   * Pick a default response to use if nothing else fits.
+   * @return a non-committal string
+   */
+  private String getRandomResponse()
+  {
+    final int NUMBER_OF_RESPONSES = 6;
+    double r = Math.random();
+    int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+    String response = "";
 
-		if (whichResponse == 0)
-		{
-			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
-			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
-			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
-			response = "You don't say.";
-		}
+    if (whichResponse == 0)
+    {
+      response = "Interesting, tell me more.";
+    }
+    else if (whichResponse == 1)
+    {
+      response = "Hmmm.";
+    }
+    else if (whichResponse == 2)
+    {
+      response = "Do you really think so?";
+    }
+    else if (whichResponse == 3)
+    {
+      response = "You don't say.";
+    }
     else if (whichResponse == 4)
     {
       response = "Okay...";
@@ -116,6 +116,6 @@ public class Magpie2
       response = "And?";
     }
 
-		return response;
-	}
+    return response;
+  }
 }
