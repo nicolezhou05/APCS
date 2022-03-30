3d7b0a0af951706dc6317e4b2a69bad98aadf44e
Nicole <nzhou30@stuy.edu>
2022-03-29 21:17:01 -0400

    Update 84_stack

--- 84_stack/LatKtS.java
+++ 84_stack/LatKtS.java
@@ -1,3 +1,21 @@
+// Team: Minions (Melody Lew, Nora Miller, Nicole Zhou)
+// APCS pd06
+// HW84 -- Stack: What Is It Good For?
+// 2022-03-29
+// time spent: 1.3 hr
+
+/*
+DISCO:
+0) For allMatched(), we need to consider the case when there's still a paren
+  left over in the stack.
+1) If statements can be empty. Just continue if boolean statement is true.
+
+QCC:
+0) This was a LOT of if statements. Is there some more efficient
+  notation for writing a whole bunch of consecutive if statments?
+1) Do we have a written test this friday?
+*/
+
 /***
  * class LatKtS
  * SKELETON
@@ -5,16 +23,26 @@
  * Uses a stack to reverse a text string, check for sets of matching parens.
  **/
 
-public class LatKtS
-{
+public class LatKtS{
 
   /***
    * precondition:  input string has length > 0
    * postcondition: returns reversed string s
    *                flip("desserts") -> "stressed"
    **/
-  public static String flip( String s )
-  {
+   // initialize stack
+   // 1. Put everything in a Stack
+   // 2. Print everything in the stack
+  public static String flip( String s ){
+    Latkes toPrint = new Latkes(s.length());
+    for (int i = 0; i < s.length(); i++){ // goes through each character of s
+      toPrint.push(s.substring(i, i+1));
+    } // gets everythign in stack
+    String str = "";
+    for (int i = 0; i < s.length(); i++){
+      str += toPrint.pop();
+    }
+    return str;
   }
 
 
@@ -24,22 +52,57 @@ public class LatKtS
    *                allMatched( "([)]" )        -> false
    *                allMatched( "" )            -> true
    **/
-  public static boolean allMatched( String s )
-  {
+   // RULE: You can't close an outer set of parenthesis unless all pairs of parentheses inside
+   //       of it have already been closed.
+  public static boolean allMatched( String s ){
+    String paren, ptner;
+    Latkes open = new Latkes(s.length() / 2);
+    for (int i = 0; i < s.length(); i++){
+      // 3.5 cases:
+      // A. It's an open parenthesis -> add to Stack
+      //    It's a close parenthesis -> Does it match the parenthesis at the top of the stack?
+        // B. It does -> no issue
+        // C. It doesn't -> you have an issue
+      // D. At some point while doing this, you attempt to close parentheses that aren't there
+      paren = s.substring(i, i+1); // the current value
+      if (paren.equals("(") || paren.equals("{") || paren.equals("[")){
+        open.push(paren); // adds it to the stack
+      }
+      else { // if it's a close parenthesis
+        if (open.isEmpty() && i < s.length() - 1){
+          return false; // finishing early case
+        }
+        ptner = open.pop();
+        if ( ( paren.equals(")") && ptner.equals("(") ) ||
+             ( paren.equals("]") && ptner.equals("[") ) ||
+             ( paren.equals("}") && ptner.equals("{") ) )
+        {
+          //continue
+        }
+        else{
+          return false;
+        }
+      }
+    }
+    if (!(open.isEmpty())) {
+      return false; // if there's still open parentheses at the end
+    }
+    return true;
   }
 
 
   //main method to test
-  public static void main( String[] args )
-  {
-    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+  public static void main( String[] args ){
+
     System.out.println(flip("stressed"));
+    System.out.println(flip("swims"));
     System.out.println(allMatched( "({}[()])" )); //true
     System.out.println(allMatched( "([)]" ) ); //false
     System.out.println(allMatched( "(){([])}" ) ); //true
     System.out.println(allMatched( "](){([])}" ) ); //false
     System.out.println(allMatched( "(){([])}(" ) ); //false
     System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
       ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
   }
 
---
+++ 84_stack/Latkes.java
@@ -0,0 +1,121 @@
+/***
+ * class Latkes
+ * v1
+ * SKELETON
+ * Implements a stack of Strings using an encapsulated array
+ **/
+
+
+public class Latkes{
+  private String[] _stack;
+  private int _stackSize; // refers to the NUMBER OF ITEMS, not the capacity
+
+
+  //constructor
+  public Latkes( int initCapacity ){
+    _stackSize = 0;
+    _stack = new String[initCapacity];
+  }// O(1)
+
+
+  //means of insertion
+  public void push( String s ){
+    if (isFull()) {
+      String[] temp = new String[_stack.length * 2];
+      for (int i = 0; i < _stack.length; i++){
+        temp[i] = _stack[i];
+      }
+      _stack = temp;
+    }
+    _stack[_stackSize] = s;
+    _stackSize += 1;
+  }// O(n)
+
+
+  //means of removal
+  public String pop( ){
+    if (isEmpty()){
+      return null;
+    }
+    _stackSize -= 1;
+    String str = _stack[_stackSize];
+    _stack[_stackSize] = "";
+    return str;
+  }// O(1)
+
+  public String peek( ){
+    if (isEmpty()){
+      return null;
+    }
+    String str = _stack[_stackSize - 1];
+    return str;
+  }// O(1)
+
+
+  //chk for emptiness
+  public boolean isEmpty(){
+    if (_stackSize == 0){
+      return true;
+    }
+    return false;
+  }// O(1)
+
+
+  //chk for fullness
+  public boolean isFull(){
+    if (_stackSize == _stack.length){
+      return true;
+    }
+    return false;
+  }// O(1)
+
+
+  //main method for testing
+  public static void main( String[] args ){
+
+    Latkes tastyStack = new Latkes(10);
+    tastyStack.push("aoo");
+    tastyStack.push("boo");
+    tastyStack.push("coo");
+    tastyStack.push("doo");
+    tastyStack.push("eoo");
+    tastyStack.push("foo");
+    tastyStack.push("goo");
+    tastyStack.push("hoo");
+    tastyStack.push("ioo");
+    tastyStack.push("joo");
+    tastyStack.push("coocoo");
+    tastyStack.push("cachoo");
+    //cachoo
+    System.out.println( tastyStack.pop() );
+    //System.out.println( tastyStack._stackSize);
+    //coocoo
+    System.out.println( tastyStack.pop() );
+    //joo
+    System.out.println( tastyStack.pop() );
+    //ioo
+    System.out.println( tastyStack.pop() );
+    //hoo
+    System.out.println( tastyStack.pop() );
+    //goo
+    System.out.println( tastyStack.pop() );
+    //foo
+    System.out.println( tastyStack.pop() );
+    //eoo
+    System.out.println( tastyStack.pop() );
+    //doo
+    System.out.println( tastyStack.pop() );
+    //coo
+    System.out.println( tastyStack.pop() );
+    //boo
+    System.out.println( tastyStack.pop() );
+    //aoo
+    System.out.println( tastyStack.pop() );
+    //stack empty by now; SOP(null)
+    System.out.println( tastyStack.pop() );
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
+  }//end main()
+
+}//end class Latkes
