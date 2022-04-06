a6c6a92146c262a9f17f4163592404993cb24b91
Nicole <nzhou30@stuy.edu>
2022-04-06 01:46:09 -0400

    Welcome01 and 02

--- cobo/data/DataStudentCode/Welcome03_List.java
+++ cobo/data/DataStudentCode/Welcome03_List.java
@@ -1,3 +1,8 @@
+// Team: Non-Disclosure Agreement (Corina Chen, Jing Yi Feng, Nicole Zhou)
+// APCS
+// LAB08: America's Next Top Data Scientist
+// 2022-04-05
+
 /*
  * Arrays of objects
  */
@@ -9,11 +14,11 @@ import java.util.Scanner;
 public class Welcome03_List {
    public static void main(String[] args) {
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
-      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
+      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name",
              "station/station_id", "station/state",
              "station/latitude", "station/longitude");
       System.out.println("Total stations: " + allstns.size());
-      
+
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a state abbreviation: ");
       String state = sc.next();
--- cobo/data/README.md
+++ cobo/data/README.md
@@ -3,8 +3,11 @@
 
 #### Experience with this lab:
 
+#### Activity 3
+1. The temperature at Atlanta, Hartsfield - Jackson Atlanta International Airport, GA is 61.0F
+2. KNYC; The temperature at New York City, Central Park, NY is 46.0F
 
 Links:
-https://w1.weather.gov/xml/current_obs/
-https://github.com/berry-cs/sinbad/blob/master/tutorials/java/welcome02-obj.md
-https://w1.weather.gov/xml/current_obs/
+* https://w1.weather.gov/xml/current_obs/
+* https://github.com/berry-cs/sinbad/blob/master/tutorials/java/welcome02-obj.md
+* https://github.com/berry-cs/sinbad/blob/master/tutorials/java/welcome03-objs.md
